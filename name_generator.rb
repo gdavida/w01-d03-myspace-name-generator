4c7f527e52ec560bb9236b877ddf6a06d3230572
Davida Gaffney <gdavida@gmail.com>
2016-03-16 17:39:36 -0500

    unshift xX_ to add of front of array and << (push) Xx_ to add to end of array

--- name_generator.rb
+++ name_generator.rb
@@ -17,7 +17,17 @@ require "pry"
 #
 #		xX_DaViDaGaFfNeY_Xx
 #
-#
+# #
+
+# a = [ "a", "b", "c" ]
+# a.each_index {|x, y|  y.upcase }
+
+
+# arr = [1, 2, 3, 4, 5, 6]
+# arr.select { |a| a > 3 }     #=> [4, 5, 6]
+# arr.reject { |a| a < 3 }     #=> [3, 4, 5, 6]
+# arr.drop_while { |a| a < 4 } #=> [4, 5, 6]
+# arr                          #=> [1, 2, 3, 4, 5, 6]
 
 
 ### 1 take strings and downcase
@@ -34,15 +44,16 @@ end
 
 
 ### 3 take array of lowercase and turn into array with array(odd) upcase
-
-
+def add_uppercase_to_array(name_to_array)
+end
 
 
 
 ### 4 unshift xX_ to add of front of array and << (push) Xx_ to add to end of array
-
-
-
+def cased_array(name_to_array)
+	name_to_array.unshift("xX_")
+	name_to_array<<"_Xx"
+end
 
 ### 5 turn back into string
 
@@ -51,5 +62,3 @@ end
 ### xX_DaViDaGaFfNeY_Xx
 
 binding.pry
-
-
--- tests/name_generator_test.rb
+++ tests/name_generator_test.rb
@@ -4,6 +4,22 @@ require_relative '../lib/name_generator.rb'
 
 # Now run your actual tests, making sure to do output as necessary
 
+
+def assert(bool, message)
+  if bool
+    puts ":)"
+  else
+    puts message
+  end
+end
+
+def refute(bool, message)
+  if !bool
+    puts ":)"
+  else
+    puts message
+  end
+end
  
 # Andrew Ek => xX_AnDrEwEk_Xx
 # davida Gaffney => xX_DaViDaGaFfNeY_Xx
@@ -11,3 +27,20 @@ require_relative '../lib/name_generator.rb'
 # xX__Xx
 # xX__Xx
 # xX__Xx
+
+
+# FIRST TEST
+result = my_name("Davida", "Gaffney")
+assert(result, "xX_DaViDaGaFfNeY_Xx")
+
+# # SECOND TEST
+result = validate_triangle("Andrew" "Ek")
+assert(result, "xX_AnDrEwEk_Xx")
+
+# # THIRD TEST
+# result = validate_triangle(5, 6, 12)
+# refute(result, "5,6,12 passed, but should have failed")
+
+# # Fourth Test
+# result = validate_triangle(12, 6, 5)
+# refute(result, "12, 6, 5 passed, but should have failed")
