cc784162b3e8c146bfa3ea3e156b919fbd2ea9db
Davida Gaffney <gdavida@gmail.com>
2016-03-17 12:21:43 -0500

    Testing works

--- tests/name_generator_test.rb
+++ tests/name_generator_test.rb
@@ -1,4 +1,4 @@
-require_relative '../lib/name_generator.rb'
+require_relative '../lib/myspace_name_generator.rb'
 
 # Define whatever methods you need first
 
@@ -20,27 +20,51 @@ def refute(bool, message)
     puts message
   end
 end
- 
-# Andrew Ek => xX_AnDrEwEk_Xx
-# davida Gaffney => xX_DaViDaGaFfNeY_Xx
-# tonyaCarpenter => xX_ToNyAcArPeNtEr_Xx
-# xX__Xx
-# xX__Xx
-# xX__Xx
+
+# Think about:
+
+# Valid strings
+# All-Caps strings
+# Lower-case strings
+# Strings with mostly punctuation
+# Empty Strings
+# Strings containing numbers
+
+
 
 
 # FIRST TEST
-result = my_name("Davida", "Gaffney")
-assert(result, "xX_DaViDaGaFfNeY_Xx")
+# my_name("Davida", "Gaffney")
+# expect "xX_DaViDaGaFfNeY_Xx"
+result = generate_myspace_name("Davida", "Gaffney")
+assert("xX_DaViDaGaFfNeY_Xx" == result, "expected true, but your method returned false")
 
-# # SECOND TEST
-result = validate_triangle("Andrew" "Ek")
-assert(result, "xX_AnDrEwEk_Xx")
 
-# # THIRD TEST
-# result = validate_triangle(5, 6, 12)
-# refute(result, "5,6,12 passed, but should have failed")
+# # # SECOND TEST
+# my_name("TONYA", "CARPENTER")
+# expect "xX_ToNyAcArPeNtEr_Xx"
+result = generate_myspace_name("TONYA", "CARPENTER")
+assert("xX_ToNyAcArPeNtEr_Xx" == result, "expected true, but your method returned false")
 
-# # Fourth Test
-# result = validate_triangle(12, 6, 5)
-# refute(result, "12, 6, 5 passed, but should have failed")
+
+# # # THIRD TEST
+# my_name("andrew", "ek")
+# "xX_AnDrEwEk_Xx"
+result = generate_myspace_name("andrew", "ek")
+assert("xX_AnDrEwEk_Xx" == result, "expected true, but your method returned false")
+
+
+# # # Fourth Test
+# my_name("123", "4567")
+# expect "xX_1234567_Xx"
+result = generate_myspace_name("123", "4567")
+assert("xX_1234567_Xx" == result, "expected true, but your method returned false")
+
+
+# # # Fifth Test
+# my_name("", "")
+# expect "xX__Xx"
+result = generate_myspace_name("", "")
+assert("xX__Xx" == result, "expected true, but your method returned false")
+
+
